477c181e685d7c49b1037c56e9299d534cccb74d
Kohlton Bingham <kohltonhbingham@gmail.com>
2023-02-13 20:15:51 -0700

    Add project files.

---
+++ Mission06_kbing321/Models/MovieContext.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission06_kbing321.Models
+{
+    public class MovieContext : DbContext 
+    {
+        //  constructor
+        public MovieContext(DbContextOptions<MovieContext> options) : base (options)
+        {
+            
+        }
+
+        public DbSet<MovieEntryForm> Movies { get; set; }
+
+        // seed first three movies to db
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<MovieEntryForm>().HasData(
+                new MovieEntryForm
+                {
+                    MovieId = 1,
+                    Category = "Action/Adventure",
+                    Title = "Inception",
+                    Year = 2010,
+                    Director = "Christopher Nolan",
+                    Rating = "PG-13",
+                    Edited = false,
+                    LentTo = "",
+                    Notes = ""
+                },
+                new MovieEntryForm
+                {
+                    MovieId = 2,
+                    Category = "Drama",
+                    Title = "The Prestige",
+                    Year = 2006,
+                    Director = "Christopher Nolan",
+                    Rating = "PG-13",
+                    Edited = false,
+                    LentTo = "",
+                    Notes = ""
+                },
+                new MovieEntryForm
+                {
+                    MovieId = 3,
+                    Category = "Action/Adventure",
+                    Title = "Spider-man: No Way Home",
+                    Year = 2021,
+                    Director = "Jon Watts",
+                    Rating = "PG-13",
+                    Edited = false,
+                    LentTo = "",
+                    Notes = ""
+                }
+            );
+        }
+    }
+}
